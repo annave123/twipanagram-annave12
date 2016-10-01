37b3904db63dd7e68adecc3f5741bd5fdad155ae
Anthony N <tony.nave@aol.com>
2016-09-30 19:07:19 -0700

    hello

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,30 @@
 package com.company;
 
-public class Main {
 
-    public static void main(String[] args) {
-	// write your code here
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Main{
+    public static boolean anagram(String str1, String str2) {
+
+
+        char[] FirstWord = str1.toLowerCase().replaceAll("\\W", "").toCharArray();
+        char[] SecondWord = str2.toLowerCase().replaceAll("\\W", "").toCharArray();
+        Arrays.sort(FirstWord);
+        Arrays.sort(SecondWord);
+        return Arrays.equals(FirstWord, SecondWord);
+    }
+
+
+    public static void main (String [] args) {
+        Scanner why = new Scanner(System.in);
+        System.out.println("Enter your first word");
+        String h = why.nextLine();
+        System.out.println("Enter your second word");
+        String j = why.nextLine();
+
+        System.out.println(anagram(h, j));
+
     }
 }
+
